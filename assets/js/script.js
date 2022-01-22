37a537cd92f21d8b8b71c8c191a17741d3e193d2
Joanna Christian <joanna.s.christian@gmail.com>
2022-01-21 21:37:09 -0800

    created render history function and create button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,67 @@
+//require('dotenv').config();
+
 let fetchButton = document.getElementById('fetch-button');
 let input = document.querySelector('.inputValue');
-let searchLocation = document.getElementById('searchlocation');
-let todayTemp = document.getElementById('todaytemp');
-let todayWind = document.getElementById('todaywind');
-let todayHumidity = document.getElementById('todayhumidity');
 let todayUv = document.getElementById('todayuv');
-let icon = document.getElementById('icon');
-
+let searchHistoryEl = document.getElementById('searchHistory')
 
 const apiKey = "b0ce11bf2a4fc3e650c06aa56d4e2b0c";
 
 fetchButton.addEventListener('click', function() {
     let inputValue = input.value;
+    populateDisplay(inputValue);
+    searchHistory(inputValue);
+});
 
-//REQUEST CURRENT WEATHER
+//Render Search History 
+
+function searchHistory (pizza) {
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+    if (history.indexOf(pizza) !== -1) return;
+    history.push(pizza)
+    localStorage.setItem('history', JSON.stringify(history))
+    renderHistoryButton();
+}
+
+//Create Buttons from Search History
+
+function renderHistoryButton () {
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+    searchHistoryEl.innerHTML= "";
+    history.forEach(function (element) {
+        let tempButton = document.createElement('button');
+        tempButton.textContent = element;
+        searchHistoryEl.append(tempButton);
+    })
+}
+
+function populateDisplay (inputValue) {
     const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=imperial`;
 
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
-      })
+    })
     .then(function (data) {
         console.log(data);
 
-        let locationSearch = document.createElement('h2');
+        let locationSearch = document.getElementById('city');
         locationSearch.textContent = data.name;
-        searchLocation.append(locationSearch);
 
         const icon = `http://openweathermap.org/img/wn/${
             data.weather[0]["icon"]
-          }.png`;
-        let iconImg = document.createElement('img');
+        }.png`;
+        let iconImg = document.getElementById('icon');
         iconImg.src = icon;
-        searchLocation.append(iconImg);
-        console.log(iconImg)
 
-        let tempToday = document.createElement('h4');
-        tempToday.textContent = 'Temp: ' + data.main.temp; + '\xB0F';
-        todayTemp.append(tempToday)
+        let tempToday = document.getElementById('temp');
+        tempToday.textContent =  data.main.temp + '\xB0 F';
+//
+        let windToday = document.getElementById('wind');
+        windToday.textContent = data.wind.speed + 'MPH';
 
-        let windToday = document.createElement('h4');
-        windToday.textContent = 'Wind: ' + data.wind.speed + 'MPH';
-        todayWind.append(windToday)
-
-        let humidityToday = document.createElement('h4');
-        humidityToday.textContent = 'Humidity: ' + data.main.humidity + '%';
-        todayHumidity.append(humidityToday);
+        let humidityToday = document.getElementById('humidity');
+        humidityToday.textContent = data.main.humidity + '\%';
 
         let latitudeValue = data.coord.lat
         let longitudeValue = data.coord.lon
@@ -55,25 +71,42 @@ fetchButton.addEventListener('click', function() {
         fetch (uvUrl)
         .then(function (response) {
             return response.json();
-          })
+        })
         .then(function (uvdata) {
             console.log(uvdata);
 
-            let uvToday = document.createElement('h4');
-            uvToday.textContent = 'UV Index: ' + uvdata.current.uvi;
-            todayUv.append(uvToday)    
+            let uvIndex = uvdata.current.uvi;
+            let uvToday = document.getElementById('uvi');
+            uvToday.textContent = uvIndex;
+            
+            if (uvIndex <= 2) {
+                uvToday.style.backgroundColor = "green";
+            }
+            else if  (uvIndex <= 5 && uvIndex >= 3 ){
+                uvToday.style.backgroundColor = "yellow";
+            }    
+            else if  (uvIndex <= 7 && uvIndex >= 6 ){
+                uvToday.style.backgroundColor = "orange";
+            }    
+            else {
+                uvToday.style.backgroundColor = "red";
+            }    
+            todayUv.append(uvToday)  
         });
-      });
+    });
+
+    //REQUEST 5 DAY FORECAST
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${apiKey}&units=imperial`;
+
+    fetch(forecastUrl)
+    .then(function (r) {
+        return r.json();
+    })
+    .then(function (forecast) {
+        console.log(forecast);
+    });
+
+}
 
 
-//REQUEST 5 DAY FORECAST
-      const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${apiKey}&units=imperial`;
 
-      fetch(forecastUrl)
-      .then(function (r) {
-          return r.json();
-      })
-      .then(function (forecast) {
-          console.log(forecast);
-      });
-});
